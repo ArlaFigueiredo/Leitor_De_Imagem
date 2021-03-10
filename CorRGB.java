d20f13db923bbf7ff86791f2d59225be07aaecfa
Rafael Campos <rafacampos2000@hotmail.com>
2021-03-10 13:32:40 -0300

    Implementando o metódo de busca da luminosidade

--- CorRGB.java
+++ CorRGB.java
@@ -3,4 +3,13 @@ public class CorRGB
     private int red;
     private int green;
     private int blue;
+    private double luminosidade;
+    
+    
+    public double getLuminosidade(){
+        this.luminosidade = Math.ceil(( this.red * 0.3 +
+                                        this.green * 0.59 + 
+                                        this.blue * 0.11   )/255);
+         return luminosidade;
+    }
 }
