aa49a7c0a5afb8e2c017ddef914724de496f39e0
Maria Isis <57303255+mariaisiss@users.noreply.github.com>
2021-03-15 13:10:59 -0300

    implementando os métodos para modificar e acessar cada componente da Cor RGB

--- CorRGB.java
+++ CorRGB.java
@@ -1,10 +1,33 @@
-public class CorRGB
+ public class CorRGB
 {
     private int red;
     private int green;
     private int blue;
     private double luminosidade;
     
+    public int getRed(){
+        return this.red;
+    }    
+    
+    public int getGreen(){
+        return this.green;
+    }
+    
+    public int getBlue(){
+        return this.blue;
+    }
+
+    public void setRed(int red){
+        this.red = red;
+    }    
+    
+    public void setGreen(int green){
+        this.green = green;
+    }
+    
+    public void setBlue(int blue){
+        this.blue = blue;
+    }
     
     public double getLuminosidade(){
         this.luminosidade = Math.ceil(( this.red * 0.3 +
