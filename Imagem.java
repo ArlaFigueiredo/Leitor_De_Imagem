3109d511feb7888f2c227ba7cf6d07773b084894
Arla Figueiredo <52615424+ArlaFigueiredo@users.noreply.github.com>
2021-04-21 14:11:02 -0300

    Simplificaçao2 dos metodos de comparação de images

--- Imagem.java
+++ Imagem.java
@@ -46,42 +46,22 @@ public class Imagem
     }
 
     public boolean equals(Imagem imagem){
+
         int cont = 0;
-        boolean vira = false;
-        boolean retorno = false;
 
         while(cont < 4){
-            for (int i = 0; i < this.getAltura(); i++){
-                // Se a altura ou a largura for diferente já tenta virar
-                if((this.getAltura() != imagem.getAltura()) || (this.getLargura() != imagem.getLargura())){
-                    vira = true;
-                }
-                else{
-                    for (int j = 0; j < this.getLargura(); j++){
-                        // Se achar um pixel diferente, irá tentar virar a imagem
-                        if((this.imagem[i][j].equals(imagem.getPixel(j, i))) == false){
-                            vira = true;
-                            break;
-                        }
-                        // Se conseguiu chegar ao final da imagem, é por que são iguais
-                        if(i == (this.getAltura() - 1) && j == (this.getLargura() - 1)){
-                            retorno = true;
-                        }
-                    }
-                }
-                // Vira a imagem e reeinicia a verificação 
-                if(vira){
-                    imagem = viraImagem(imagem);
-                    cont++;
-                    vira = false;
-                    break;
-                }
-            }
+            if(estaContida(imagem, 0, 0))
+                return true;
+
+            // Vira a imagem
+            imagem = viraImagem(imagem);
+            cont++;
         }
-        return retorno;
+        return false;
     }
 
     public Imagem novaImagemPB(){
+        
         Imagem novaImagemPB = new Imagem(this.getLargura(), this.getAltura());        
         for (int i = 0; i < novaImagemPB.getAltura(); i++){
             for (int j = 0; j < novaImagemPB.getLargura(); j++){
@@ -90,83 +70,35 @@ public class Imagem
         }       
         return novaImagemPB;
     }
+    
+    public boolean isFragment(Imagem fragmento){ 
 
-    private int[] procuraPixel(int controlador[], Imagem fragmento){
-        boolean stop = false;
-        for (int i = controlador[1]; i < this.getAltura(); i++){
-            for (int j = controlador[0]; j < this.getLargura(); j++){
-                // Verifica se achou o primeiro pixel igual e se o fragmento ainda cabe na imagem
-                if ((this.getPixel(j,i).equals(fragmento.getPixel(0,0))) && (i+fragmento.getAltura()<=this.getAltura()) && (j+fragmento.getLargura()<=this.getLargura())){
-                    controlador[0] = j; //armazena a largura do pixel achado na imagem
-                    controlador[1] = i; //armazena a altura do pixel achado na imagem
-                    stop = true;
-                    break;
-                }
-                if(i == (this.getAltura()-1) && j==(this.getLargura()-1)){
-                    // Se não achou nada, coloca -1 nas posições como um indicador
-                    controlador[0] = -1; 
-                    controlador[1] = -1;
-                }
-            }
-            if(stop)
-                break;
-        }
-        return controlador;
-    }
-
-    public boolean verificaFragmento(Imagem fragmento){ 
         int cont = 0;
-        boolean retorno = false; //retorno lógicboolean ecx = false; // trata exceções
-        int[] controlador = new int[2];//variável de retorno de posições da imagem + retorno lógico da função procuraPixel()
-        boolean vira = false;
-        
-        
         while(cont < 4){
-            boolean continua_busca = true;
-            controlador[0] = 0;
-            controlador[1] = 0;            
-            while(continua_busca == true){
-                boolean stop = false;
-                // procura a posição na imagem cujo pixel é igual igual ao Pixel (0,0) do fragmento 
-                controlador = procuraPixel(controlador, fragmento);
-                // A função procuraPixel() achou uma posição válida
-                if(controlador[0] != -1){
-                    for (int iFrag = 0, iImag = controlador[1]; iFrag < fragmento.getAltura(); iFrag++, iImag++){
-                        for (int jFrag = 0, jImag = controlador[0]; jFrag < fragmento.getLargura(); jFrag++, jImag++){
-                            if(!this.getPixel(jImag, iImag).equals(fragmento.getPixel(jFrag, iFrag))){
-                                // Caso a posicao do controlador de largura é a ultima coluna da imagem, vai para proxima linha
-                                if(controlador[0] == this.getLargura()-1){
-                                    stop = true;
-                                    controlador[1] =+ 1;
-                                    controlador[0] = 0;
-                                    break;
-                                }
-                                else{
-                                    controlador[0] += 1;
-                                    stop = true;
-                                    break;
-                                }                       
-                            }
-                            if ((iFrag == (fragmento.getAltura() - 1)) && (jFrag == (fragmento.getLargura() - 1))){
-                                retorno = true;
-                                continua_busca = false;
-                                cont  = 4;
-                            }
-                        }
-                        if(stop)
-                            break; //Quebra segundo laço
-                    }
-                }else{ // O retorno da função procuraPixel() foi negativo, vira a imagem
-                    vira = true;
-                    continua_busca = false;
-                }
-                if(vira){
-                    fragmento = viraImagem(fragmento);
-                    vira = false;
-                    cont++;
+            for (int iImag = 0; iImag < this.getAltura(); iImag++){
+                for (int jImag = 0; jImag < this.getLargura(); jImag++){
+                    if(estaContida(fragmento, jImag, iImag))
+                        return true;
                 }
             }
+            // Vira o fragmento
+            fragmento = viraImagem(fragmento);
+            cont++;
         }
-        return retorno;
+        return false;
     }            
+
+    private boolean estaContida(Imagem imagem, int largura, int altura){ 
+
+        if ((altura + imagem.getAltura() > this.getAltura()) || (largura + imagem.getLargura() > this.getLargura()))
+            return false;
+
+        for (int iFrag = 0, iImag = altura; iFrag < imagem.getAltura(); iFrag++, iImag++){
+            for (int jFrag = 0, jImag = largura; jFrag < imagem.getLargura(); jFrag++, jImag++){
+                if(!this.getPixel(jImag, iImag).equals(imagem.getPixel(jFrag, iFrag)))
+                    return false;
+            }
+        }
+        return true;
+    }
 }
