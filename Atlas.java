7bd48e83a49e7cf7d54f13ea0a443616a2d856de
Rafael Campos <rafacampos2000@hotmail.com>
2021-06-28 15:37:51 -0300

    Adicionando método de listagem de mapas

--- Atlas.java
+++ Atlas.java
@@ -18,12 +18,19 @@ public class Atlas{
         Atlas mapasSimilares = new Atlas();
 
         for(int i = 0; i < this.mapas.length; i++){
-            if(this.mapas[i].getPercentualPixelsSimilares(new CorRGB(red, green, blue), double limiarLuminosidade) >= pctMinimo){
+            if(this.mapas[i].getPercentualPixelsSimilares(new CorRGB(red, green, blue),  limiarLuminosidade) >= pctMinimo){
                 mapasSimilares.adicionaMapa(this.mapas[i]); 
             }
             
         } 
         return mapasSimilares;     
     };
+    
+    
+    public void listarMapas() {
+    	for(Mapa mapa: mapas) {
+    		System.out.println(mapa.getDescricao());
+    	}
+    }
     
 }
