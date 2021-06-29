62146123020946fd430def89ff0d2dc57ef96608
Rafael Campos <rafacampos2000@hotmail.com>
2021-06-29 17:12:56 -0300

    Reorganizando variaveis e consertando tipagem

--- Sistema.java
+++ Sistema.java
@@ -1,7 +1,12 @@
 public class Sistema{
 
     private Atlas atlas;
-
+    private MapaRGB mapa1;
+    private MapaRGB mapa2;
+    private MapaRGB mapa3;
+    private MapaCMYK mapa4;
+    private MapaCMYK mapa5;
+    private MapaCMYK mapa6;
 
     public Sistema(){
         
@@ -12,13 +17,13 @@ public class Sistema{
         
             CaptadorDeImagens captador = new CaptadorDeImagens();
             
-            MapaRGB mapa1 = new MapaRGB("EST01","Estrada da Lama",captador.captaEstrada());
-            MapaRGB mapa2 = new MapaRGB("PNT68","Ponte Rio São Francisco",captador.captaPonte());
-            MapaRGB mapa3 = new MapaRGB("FLR90","Floresta Amazônia",captador.captaFloresta());
+            mapa1 = new MapaRGB("EST01","Estrada da Lama",captador.captaEstrada());
+            mapa2 = new MapaRGB("PNT68","Ponte Rio São Francisco",captador.captaPonte());
+            mapa3 = new MapaRGB("FLR90","Floresta Amazônia",captador.captaFloresta());
           
-            MapaCMYK mapa4 = new MapaCMYK("PRT45","Porto Rio Douro",captador.captaPorto());
-            MapaCMYK mapa5 = new MapaCMYK("PRA89","Praia do Forte",captador.captaPraia());
-            MapaCMYK mapa6 = new MapaCMYK("RDV03","Rodovia 324",captador.captaRodovia());
+            mapa4 = new MapaCMYK("PRT45","Porto Rio Douro",captador.captaPorto());
+            mapa5 = new MapaCMYK("PRA89","Praia do Forte",captador.captaPraia());
+            mapa6 = new MapaCMYK("RDV03","Rodovia 324",captador.captaRodovia());
            
             
             this.atlas.adicionaMapa(mapa1);
@@ -31,20 +36,21 @@ public class Sistema{
     }
 
     public void run(){
-        for(Mapa m : this.atlas)
-           System.out.println(m);
-
+        for(Mapa mapa : this.atlas.getMapas())
+        	System.out.println(mapa);
         //TODO IMPLEMENTAR A BUSCA POR SIMILARIDADE DOS MAPAS RGB ACIMA
-
-        // mapasSimilares = this.atlas.getMapasPorLuminosidade();
+        
+        System.out.println("Mapas Similares :");
+        Mapa[] mapasSimilares = this.atlas.getMapasPorLuminosidade(121,85,72,20,30);
+        for(Mapa mapaSimilar : mapasSimilares)
+        	System.out.println(mapaSimilar);
         
         //TODO IMPLEMENTAR A CONVERSÃO DOS MAPAS CMYK PARA RGB
 
         Conversor conversor = new Conversor();
         ConversorCMYKToRGB conversorCMYKToRGB = new ConversorCMYKToRGB();
 
-        MapaRGB mapa4rgb = conversor.converter(mapa4, conversorCMYKToRGB);
-        System.out.println(mapa4rgb);
+        MapaRGB mapa5rgb = (MapaRGB) conversor.converter(mapa5, conversorCMYKToRGB);
 
     }
     
