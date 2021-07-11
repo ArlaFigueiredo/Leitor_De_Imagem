c6d87e1157b4418097a4ac967179652d7a4ca19e
ArlaFigueiredo <arla.figueiredo@gmail.com>
2021-07-11 10:12:48 -0300

    Ajustante User Interface

--- FINAL/src/sistema/ui/JSistemaUI.java
+++ FINAL/src/sistema/ui/JSistemaUI.java
@@ -5,30 +5,31 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.io.IOException;
+//import java.io.IOException;
 import java.util.Collection;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
+//import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JFileChooser;
 
-import sistema.model.Sistema;
+//import sistema.model.Sistema;
 //import sistema.model.SistemaLogicaIF;
 
 public class JSistemaUI extends JFrame implements ActionListener{
 	
 	//private SistemaLogicaIF logica;
-	private Collection<String> elementos;
+	//private Collection<String> elementos;
 	
 	private JButton btnAnalisar;
+	private JButton btnBrowser;
 	//private JComboBox<String> cmbElementos;	
-	private String[] elementosStrings = { "Floresta", "Aguas e Charcos", "Area Proibida", "Zona Constuida", "Area Publica" };
-	private JComboBox elementoList = new JComboBox(this.elementosStrings);
+	private JComboBox elementoList;
+	private JTextField txtCaminho;
 	
 	/*
 	public JSistemaUI() throws Exception {
@@ -36,12 +37,45 @@ public class JSistemaUI extends JFrame implements ActionListener{
 		//this.nomeElementos = this.logica.getNomeElementos();
 	}
 	*/
-	protected void montar() {
+	
+	private void initComponents(){
+		// Botoes
+		this.btnBrowser = new JButton("Browser");
+		this.btnAnalisar = new JButton("Analisar"); 
+		
+		// TextFilds
+		this.txtCaminho = new JTextField(); 
+		
+		// DropDown
+		String[] elementosStrings = { "Floresta", "Aguas e Charcos", "Area Proibida", "Zona Constuida", "Area Publica" };
+		this.elementoList = new JComboBox(elementosStrings);
+		
+    }
+	
+	protected void configLayout(){
+		
+		// Incluindo areas da tela
 		JPanel jPanelCenter = new JPanel();
 		JPanel jPanelSouth = new JPanel();
-		this.btnAnalisar = new JButton("Analisar"); 
-		this.btnAnalisar.addActionListener(this);
 		
+		// Montando parte de cima
+		jPanelCenter.setLayout(new GridLayout(3, 3));
+		jPanelCenter.add(new JPanel());
+		jPanelCenter.add(new JPanel());
+		jPanelCenter.add(new JPanel());
+		
+		// Inclusor do caminho da imagem
+		jPanelCenter.add(new JLabel("Informe o caminho do arquivo para avaliação:"));
+		jPanelCenter.add(this.txtCaminho);
+		jPanelCenter.add(btnBrowser);
+		
+		
+		// Incluindo dropdown elementos
+		jPanelCenter.add(new JPanel());
+		jPanelCenter.add(new JLabel("Selecione o tipo de elemento:"));
+		//this.cmbElementos = new JComboBox<String>();
+		//this.loadCombo(this.cmbElementos);
+		jPanelCenter.add(this.elementoList);
 		
 		//Montando o panel na area sul onde vai ancorar o botão
 		jPanelSouth.setLayout(new GridLayout(2, 3));
@@ -49,47 +83,46 @@ public class JSistemaUI extends JFrame implements ActionListener{
 		jPanelSouth.add(new JPanel());
 		jPanelSouth.add(new JPanel());
 		jPanelSouth.add(new JPanel());
+		
 		jPanelSouth.add(btnAnalisar);
 		jPanelSouth.add(new JPanel());
 		
-		JFileChooser fileChooser = new JFileChooser();
-		fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
-		int result = fileChooser.showOpenDialog(this);
-		if (result == JFileChooser.APPROVE_OPTION) {
-		    File selectedFile = fileChooser.getSelectedFile();
-		    System.out.println("Selected file: " + selectedFile.getAbsolutePath());
-		}
-		
-		
-		//Montando a area central com os campos (Labels e Texts)
-		jPanelCenter.setLayout(new GridLayout(1, 2));
-		jPanelCenter.add(new JLabel("Selecione o tipo de elemento:"));
-		//this.cmbElementos = new JComboBox<String>();
-		//this.loadCombo(this.cmbElementos);
-		jPanelCenter.add(this.elementoList);		
-		
-		
+		// Setando layout
 		this.getContentPane().setLayout(new BorderLayout());
 		this.getContentPane().add(jPanelCenter, BorderLayout.CENTER);
 		this.getContentPane().add(jPanelSouth, BorderLayout.SOUTH);
-//		this.getContentPane().add(btnRegistrar, BorderLayout.SOUTH);
-		
 		this.setTitle("Analisar Mapa");
-		this.setSize(800, 600);
+		this.setSize(800, 600);	
 	}
 	
+	
+	protected void configEvents() {
+		this.btnAnalisar.addActionListener(this);
+		this.btnBrowser.addActionListener(this);
+	}
+
 	public void run() {
-		this.montar();
+		initComponents();
+        configEvents();
+        configLayout();
 		this.setVisible(true);
-	}	
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+			
+		if( e.getSource() == this.btnBrowser){
+			JFileChooser fileChooser = new JFileChooser();
+			fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+			int result = fileChooser.showOpenDialog(this);
+			if (result == JFileChooser.APPROVE_OPTION) {
+			    File selectedFile = fileChooser.getSelectedFile();
+			    this.txtCaminho.setText(selectedFile.getAbsolutePath());
+			}
+		}
+	}
 	
 	public static void main(String[] args) throws Exception {
 		(new JSistemaUI()).run();  
 	}
 
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}	
-	
 }
