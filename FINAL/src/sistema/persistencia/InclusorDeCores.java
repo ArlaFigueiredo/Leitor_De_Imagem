ba4fc1c6c90056a4df7d0511516a6037822d7c07
ArlaFigueiredo <arla.figueiredo@gmail.com>
2021-07-11 16:16:57 -0300

    Ajusta class para popular cores e simbolos

--- FINAL/src/sistema/persistencia/InclusorDeCores.java
+++ FINAL/src/sistema/persistencia/InclusorDeCores.java
@@ -5,36 +5,47 @@ import java.sql.SQLException;
 import sistema.model.entity.CorCMYK;
 import sistema.model.entity.CorRGB;
 import sistema.persistencia.CorDAOSQL;
+import sistema.model.entity.Simbolo;
+
 
 public class InclusorDeCores {
     
     public void populandoTabelaCores() throws Exception{
+    	
+    	// Criando objetos simbolos
+    	Simbolo floresta = new Simbolo(1, "FLORESTA");
+    	Simbolo aguaECharcos = new Simbolo(2, "AGUAS E CHARCOS");
+    	Simbolo areaProibida = new Simbolo(3, "AREA PROIBIDA");
+    	Simbolo zonaConstuida = new Simbolo(4, "ZONA CONSTRUIDA");
+    	Simbolo areaPublica = new Simbolo(5, "AREA PUBLICA"); 
+    	
+    	
     	CorDAOSQL corDAO = new CorDAOSQL();
     	
-    	corDAO.inserir(new CorRGB("V01", "FLORESTA","VERDE DENSO", 36, 79, 20));
-    	corDAO.inserir(new CorRGB("V02", "FLORESTA","VERDE MEDIO", 43, 171, 63));
-    	corDAO.inserir(new CorCMYK("V03", "FLORESTA","VERDE ESPARSO", 64, 13, 93, 7));
+    	corDAO.inserir(new CorRGB("V01", floresta,"VERDE DENSO", 36, 79, 20));
+    	corDAO.inserir(new CorRGB("V02", floresta,"VERDE MEDIO", 43, 171, 63));
+    	corDAO.inserir(new CorCMYK("V03", floresta,"VERDE ESPARSO", 64, 13, 93, 7));
     	
-	    corDAO.inserir(new CorRGB("V04", "AGUAS E CHARCOS","AZUL MAR", 49, 81, 131));
-		corDAO.inserir(new CorRGB("V05", "AGUAS E CHARCOS","AZUL RIO", 22, 122, 175));
-		corDAO.inserir(new CorRGB("V06", "AGUAS E CHARCOS","AZUL MANGUE", 49, 118, 131));
-		corDAO.inserir(new CorCMYK("V07", "AGUAS E CHARCOS","AZUL PISCINA", 64, 13, 3, 7));
-		corDAO.inserir(new CorCMYK("V08", "AGUAS E CHARCOS","AZUL LAGO", 86, 16, 0, 30));
+	    corDAO.inserir(new CorRGB("V04", aguaECharcos,"AZUL MAR", 49, 81, 131));
+		corDAO.inserir(new CorRGB("V05", aguaECharcos,"AZUL RIO", 22, 122, 175));
+		corDAO.inserir(new CorRGB("V06", aguaECharcos,"AZUL MANGUE", 49, 118, 131));
+		corDAO.inserir(new CorCMYK("V07", aguaECharcos,"AZUL PISCINA", 64, 13, 3, 7));
+		corDAO.inserir(new CorCMYK("V08", aguaECharcos,"AZUL LAGO", 86, 16, 0, 30));
 
-		corDAO.inserir(new CorRGB("V09", "AREA PROIBIDA","VERMELHO ISOLAMENTO", 255, 0, 0));
-		corDAO.inserir(new CorCMYK("V10", "AREA PROIBIDA","VERMELHO ISOLAMENTO", 0, 100, 100, 0));
+		corDAO.inserir(new CorRGB("V09", areaProibida,"VERMELHO ISOLAMENTO", 255, 0, 0));
+		corDAO.inserir(new CorCMYK("V10", areaProibida,"VERMELHO ISOLAMENTO", 0, 100, 100, 0));
 	
-		corDAO.inserir(new CorRGB("V11", "ZONA CONSTRUIDA","CINZA VIADUTO", 109, 20, 46));
-		corDAO.inserir(new CorRGB("V12", "ZONA CONSTRUIDA","MARROM EDIFICIOS", 178, 79, 0));
-		corDAO.inserir(new CorRGB("V13", "ZONA CONSTRUIDA","ROXO ESTRADA", 60, 45, 86));
-		corDAO.inserir(new CorCMYK("V14", "ZONA CONSTRUIDA","CINZA PONTE", 0, 0, 16, 50));
-		corDAO.inserir(new CorCMYK("V15", "ZONA CONSTRUIDA","MARROM CASA", 11, 36, 56, 17));
+		corDAO.inserir(new CorRGB("V11", zonaConstuida"CINZA VIADUTO", 109, 20, 46));
+		corDAO.inserir(new CorRGB("V12", zonaConstuida,"MARROM EDIFICIOS", 178, 79, 0));
+		corDAO.inserir(new CorRGB("V13", zonaConstuida,"ROXO ESTRADA", 60, 45, 86));
+		corDAO.inserir(new CorCMYK("V14", zonaConstuida,"CINZA PONTE", 0, 0, 16, 50));
+		corDAO.inserir(new CorCMYK("V15", zonaConstuida,"MARROM CASA", 11, 36, 56, 17));
 	    
-		corDAO.inserir(new CorRGB("V16", "AREA PUBLICA","VERDE JARDIM", 12, 243, 119));
-		corDAO.inserir(new CorRGB("V17", "AREA PUBLICA","ROXO MONUMENTO", 124, 86, 119));
-		corDAO.inserir(new CorCMYK("V18", "AREA PUBLICA","CINZA ACESSO", 26, 16, 0, 71));
-		corDAO.inserir(new CorCMYK("V19", "AREA PUBLICA","ROXO ESTACIONAMENTO", 30, 48, 0, 66));
-		corDAO.inserir(new CorCMYK("V20", "AREA PUBLICA","VERDE PARQUE", 95, 0, 98, 5));
+		corDAO.inserir(new CorRGB("V16", areaPublica,"VERDE JARDIM", 12, 243, 119));
+		corDAO.inserir(new CorRGB("V17", areaPublica,"ROXO MONUMENTO", 124, 86, 119));
+		corDAO.inserir(new CorCMYK("V18", areaPublica,"CINZA ACESSO", 26, 16, 0, 71));
+		corDAO.inserir(new CorCMYK("V19", areaPublica,"ROXO ESTACIONAMENTO", 30, 48, 0, 66));
+		corDAO.inserir(new CorCMYK("V20", areaPublica,"VERDE PARQUE", 95, 0, 98, 5));
 	
 	    
     }
--- FINAL/src/sistema/persistencia/InclusorDeSimbolos.java
+++ FINAL/src/sistema/persistencia/InclusorDeSimbolos.java
@@ -10,11 +10,11 @@ public class InclusorDeSimbolos {
     public void populandoTabelaSimbolos() throws Exception{
     	SimboloDAOSQL simboloDAO = new SimboloDAOSQL();
     	
-    	SimboloDAOSQL.inserir(new Simbolo(1, "FLORESTA",);
-    	SimboloDAOSQL.inserir(new Simbolo(2, "AGUAS E CHARCOS");
-    	SimboloDAOSQL.inserir(new Simbolo(3, "AREA PROIBIDA");
-    	SimboloDAOSQL.inserir(new Simbolo(4, "ZONA CONSTRUIDA");
-    	SimboloDAOSQL.inserir(new Simbolo(5, "AREA PUBLICA");  
+    	SimboloDAOSQL.inserir(new Simbolo(1, "FLORESTA",));
+    	SimboloDAOSQL.inserir(new Simbolo(2, "AGUAS E CHARCOS"));
+    	SimboloDAOSQL.inserir(new Simbolo(3, "AREA PROIBIDA"));
+    	SimboloDAOSQL.inserir(new Simbolo(4, "ZONA CONSTRUIDA"));
+    	SimboloDAOSQL.inserir(new Simbolo(5, "AREA PUBLICA"));  
     }
 
 }
